05954f2b778fcb4f4264f5fb3c60646ace6df3ad
Kundan Vyas <kundanvyas197@gmail.com>
2024-07-06 15:50:50 +0530

    added a program to make two or more  constructer at single class

---
+++ OOPS/pro3.py
@@ -0,0 +1,13 @@
+class Animal:
+    categary = "Mamaal"
+
+    def __init__(self, name):
+        self.name = name
+
+    def speck(self):
+        print(f"My name is {self.name}")
+
+Tommy = Animal("Tommy")
+Jacky = Animal("Jacky")
+Tommy.speck()
+Jacky.speck()
