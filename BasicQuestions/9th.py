a760cbe9610efedf0704f4ba10bc97267929178f
Kundan Vyas <kundanvyas197@gmail.com>
2024-07-06 13:53:27 +0530

    Added a proram to Acces elements from class and manupulate the data.

--- BasicQuestions/9th.py
+++ BasicQuestions/9th.py
@@ -6,3 +6,28 @@ print(myset)
 # Adding element to the set
 myset.add("d")
 print(myset)
+
+
+
+demo = {'kundan', 'kuldeep', 'Trilok'}
+print(demo)
+
+#Adding new name 
+
+demo.add('Karthik')
+print(demo)
+
+
+test = set(['Yuvraj', 23.54 , 'Sidharth' , 90.00])
+print(test)
+
+#Testing something
+test.add('Vasu')
+print(test)
+
+
+DoubleCheck = set(['Kunal','Vijay','Ajay'])
+print(DoubleCheck)
+
+DoubleCheck.add('Lucky')
+print(DoubleCheck)
