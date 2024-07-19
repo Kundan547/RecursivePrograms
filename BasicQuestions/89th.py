daa5f387fe64d42457f2abdb07793306521beb94
Kundan547 <Kundanvyas197@gmail.com>
2024-07-19 23:22:45 +0530

    "Added a program to perform string cancationation"

---
+++ BasicQuestions/89th.py
@@ -0,0 +1,8 @@
+l1 = [9,2,1,4,3,8,6,5,7]
+l1.sort()
+print("Original sorted list", l1)
+
+#Basically we're reverse the sorted list by using reverse keyword.
+s_l1 = sorted(l1, reverse=True)
+print("Reverse sorted list: ", s_l1)
+
