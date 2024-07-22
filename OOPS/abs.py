1143bf92a561496f739e59e48d7b5956cf576dae
Kundan547 <Kundanvyas197@gmail.com>
2024-07-22 22:57:11 +0530

    Create a abstraction program in python

---
+++ OOPS/abs.py
@@ -0,0 +1,33 @@
+from abc import ABC, abstractclassmethod
+
+class Car(ABC):
+    def show(self):
+        print("Every car has 4 wheels: ")
+    @abstractclassmethod
+    def speed():
+        pass
+
+
+
+class Toyota(Car):
+    def speed(self):
+        print("Speed is 120km/h: ")
+
+class Mahindra(Car):
+    def speed(self):
+        print("Speed is 150 km/h: ")
+
+
+#Because of abstractmethod we dont have the access to 
+# the parent class so we need to create object from child class
+#To perform changes or create  something new.
+
+obj1 = Toyota()
+obj1.show()
+obj1.speed()
+
+obj2 = Mahindra()
+obj2.show()
+obj2.speed()
+
+# Program executed succesfully.
