ed8612adb6fecff15796fc1cf9dd54dd684d1b49
Kundan547 <Kundanvyas197@gmail.com>
2024-07-22 10:19:41 +0530

    Created a polyi,orphism class which is used to perform some actions on viechle class.

---
+++ OOPS/poly1.py
@@ -0,0 +1,27 @@
+class Vehicle:
+  def __init__(self, brand, model):
+    self.brand = brand
+    self.model = model
+
+  def move(self):
+    print("Move!")
+
+class Car(Vehicle):
+  pass
+
+class Boat(Vehicle):
+  def move(self):
+    print("Sail!")
+
+class Plane(Vehicle):
+  def move(self):
+    print("Fly!")
+
+car1 = Car("Ford", "Mustang") #Create a class object
+boat1 = Boat("Ibiza", "Touring 20") 
+plane1 = Plane("Boeing", "747") 
+
+for x in (car1, boat1, plane1):
+  print(x.brand)
+  print(x.model)
+  x.move()
